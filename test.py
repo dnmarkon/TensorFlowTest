790d8aee3dc0ca5c44ec8a17cb6f15a31e80b4d1
dnmarkon <dnmarkon@gmail.com>
2017-03-11 09:34:41 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,78 @@
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# Encode text values to dummy variables(i.e. [1,0,0],[0,1,0],[0,0,1] for red,green,blue)
+def encode_text_dummy(df, name):
+    dummies = pd.get_dummies(df[name])
+    for x in dummies.columns:
+        dummy_name = "{}-{}".format(name, x)
+        df[dummy_name] = dummies[x]
+    df.drop(name, axis=1, inplace=True)
+
+
+# Encode text values to indexes(i.e. [1],[2],[3] for red,green,blue).
+def encode_text_index(df, name):
+    le = preprocessing.LabelEncoder()
+    df[name] = le.fit_transform(df[name])
+    return le.classes_
+
+
+# Encode a numeric column as zscores
+def encode_numeric_zscore(df, name, mean=None, sd=None):
+    if mean is None:
+        mean = df[name].mean()
+
+    if sd is None:
+        sd = df[name].std()
+
+    df[name] = (df[name] - mean) / sd
+
+
+# Convert all missing values in the specified column to the median
+def missing_median(df, name):
+    med = df[name].median()
+    df[name] = df[name].fillna(med)
+
+
+# Convert a Pandas dataframe to the x,y inputs that TensorFlow needs
+def to_xy(df, target):
+    result = []
+    for x in df.columns:
+        if x != target:
+            result.append(x)
+
+    # find out the type of the target column.  Is it really this hard? :(
+    target_type = df[target].dtypes
+    target_type = target_type[0] if hasattr(target_type, '__iter__') else target_type
+    print(target_type)
+
+    # Encode to int for classification, float otherwise. TensorFlow likes 32 bits.
+    if target_type in (np.int64, np.int32):
+        # Classification
+        return df.as_matrix(result).astype(np.float32), df.as_matrix([target]).astype(np.int32)
+    else:
+        # Regression
+        return df.as_matrix(result).astype(np.float32), df.as_matrix([target]).astype(np.float32)
+
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+
+
+# Regression chart, we will see more of this chart in the next class.
+def chart_regression(pred, y):
+    t = pd.DataFrame({'pred': pred.flatten(), 'y': y.flatten()})
+    t.sort_values(by=['y'], inplace=True)
+    a = plt.plot(t['y'].tolist(), label='expected')
+    b = plt.plot(t['pred'].tolist(), label='prediction')
+    plt.ylabel('output')
+    plt.legend()
+    plt.show()
+
